81e4a250544d0ad88f0e5f87d3d94ffc898e9157
mitsuhide1992 <zhangliyuan1992@126.com>
2014-12-23 16:02:21 +0800

    Revert "new"
    This reverts commit e2ae0665caa0aeb60fa2fb9a1acd785619f0b127.

--- WebRoot/js/getUNPA.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Administrator on 13-12-25.
- */
-$(document).ready(function(){
-//    alert($.cookie("userNo"));
-    $("#userNoForModify").val($.cookie("userNo"));
-    $("#oldPassword").val($.cookie("userPassword"));
-});
